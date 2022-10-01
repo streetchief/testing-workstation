21603cb863f63db876403d4241305f0eb0375937
Samuel Leavin <sam.leavin@gmail.com>
2022-10-01 10:35:35 -0400

    add semis

--- src/lib.test.js
+++ src/lib.test.js
@@ -2,6 +2,6 @@ const { myFunction } = require('./lib');
 
 test('test myFunction', () => {
     const input = `input`;
-    const output = -1
-    expect(myFunction(input)).toEqual(output)
+    const output = -1;
+    expect(myFunction(input)).toEqual(output);
 });
